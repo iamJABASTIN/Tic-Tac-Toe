c0ed045b9c86ffdf6a207ef886d2cde4ff310980
JABASTIN <itsmejabastin@gmail.com>
2024-09-07 16:59:30 +0530

    Game board is created

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,33 +1,35 @@
 import { useState } from "react";
 
-export default function Player({initialName, symbol}) {
-    const [isEditing, setIsEditing] = useState(false);
-    const [playerName, setPlayerName] = useState(initialName);
+export default function Player({ initialName, symbol }) {
+  const [isEditing, setIsEditing] = useState(false);
+  const [playerName, setPlayerName] = useState(initialName);
 
-    function handleEditClick() {
-        setIsEditing(editing => !editing);
-    }
+  function handleEditClick() {
+    setIsEditing((editing) => !editing);
+  }
 
-    function handleChange(event) {
-        console.log(event);
-        setPlayerName(event.target.value)
-    }
+  function handleChange(event) {
+    console.log(event);
+    setPlayerName(event.target.value);
+  }
 
-    let editablePlayerName = <span className="player-name">{playerName}</span>;
-    let buttonCaption = 'Edit';
+  let editablePlayerName = <span className="player-name">{playerName}</span>;
+  let buttonCaption = "Edit";
 
-    if(isEditing) {
-        editablePlayerName = <input type="text" required value={playerName} onChange={handleChange}/>;
-        buttonCaption = 'save';
-    }
+  if (isEditing) {
+    editablePlayerName = (
+      <input type="text" required value={playerName} onChange={handleChange} />
+    );
+    buttonCaption = "save";
+  }
 
-    return (
-        <li>
-            <span className="player">
-                {editablePlayerName}
-                <span className="player-symbol">{symbol}</span>
-            </span>
-            <button onClick={handleEditClick}>{buttonCaption}</button>
-        </li>
-    )
+  return (
+    <li>
+      <span className="player">
+        {editablePlayerName}
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      <button onClick={handleEditClick}>{buttonCaption}</button>
+    </li>
+  );
 }
