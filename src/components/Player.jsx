293580076e90adb784b8917f5c50c0159f487a2f
JABASTIN <itsmejabastin@gmail.com>
2024-09-07 17:29:04 +0530

    Initial Game Board functions are updated

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-export default function Player({ initialName, symbol }) {
+export default function Player({ initialName, symbol, isActive }) {
   const [isEditing, setIsEditing] = useState(false);
   const [playerName, setPlayerName] = useState(initialName);
 
@@ -9,7 +9,6 @@ export default function Player({ initialName, symbol }) {
   }
 
   function handleChange(event) {
-    console.log(event);
     setPlayerName(event.target.value);
   }
 
@@ -24,7 +23,7 @@ export default function Player({ initialName, symbol }) {
   }
 
   return (
-    <li>
+    <li className={isActive ? 'active' : undefined}>
       <span className="player">
         {editablePlayerName}
         <span className="player-symbol">{symbol}</span>
