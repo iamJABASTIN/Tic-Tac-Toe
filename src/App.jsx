2eee57fc89d3ea7320de78b8e3724b0a5c20db3d
JABASTIN <itsmejabastin@gmail.com>
2024-09-08 16:26:45 +0530

    Game functionality has been finished properly

--- src/App.jsx
+++ src/App.jsx
@@ -24,6 +24,7 @@ function deriveActivePlayer(gameTurns) {
 }
 
 function App() {
+  const [players, setPlayers] = useState({ X: "Player 1", O: "Player 2" });
   const [gameTurns, setGameTurn] = useState([]);
   // const [activePlayer, setActivePlayer] = useState("X");
 
@@ -53,7 +54,7 @@ function App() {
       firstSquareSymbol === secondSquareSymbol &&
       firstSquareSymbol === thirdSquareSymbol
     ) {
-      winner = firstSquareSymbol;
+      winner = players[firstSquareSymbol];
     }
   }
 
@@ -61,6 +62,15 @@ function App() {
 
   function handleRestart() {
     setGameTurn([]);
+  }
+
+  function handlePlayerNameChange(symbol, newName) {
+    setPlayers(prevPlayers => {
+      return {
+        ...prevPlayers,
+        [symbol]:newName
+      }
+    })
   }
 
   function handleSelectSquare(rowIndex, colIndex) {
@@ -88,12 +98,14 @@ function App() {
             initialName="Player 1"
             symbol="X"
             isActive={activePlayer === "X"}
-          />
+            onChangeName = {handlePlayerNameChange}
+            />
           <Player
             initialName="Player 2"
             symbol="O"
             isActive={activePlayer === "O"}
-          />
+            onChangeName = {handlePlayerNameChange}
+            />
         </ol>
         {(winner || hasDraw) && (
           <GameOver winner={winner} onRestart={handleRestart} />
